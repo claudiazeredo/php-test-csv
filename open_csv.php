2812f54cdb68cb5959284f0d9d2b50418dea55d5
Claudia <claudinha.tomaz@gmail.com>
2020-06-11 22:00:32 -0300

    Abertura e manipulacao de arquivo csv

--- open_csv.php
+++ open_csv.php
@@ -2,10 +2,12 @@
 
 $handle = fopen('nota.csv','r');
 
-$header = fgetcsv($handle,1000,",");
+$header = fgetcsv($handle,1000,";");
 
-while($row = fgetcsv($handle,1000,",")){
+while($row = fgetcsv($handle,1000,";")){
+    //print_r($row);
     $nota[] = array_combine($header,$row);
+
 }
 
 print_r($nota);
